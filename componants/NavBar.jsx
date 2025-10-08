a65fe2d39f7bc8999b2a20915c62ad56bb3f9c10
Mohamed Amine IRATENE <amine.iratene@MONO.Local>
2025-10-08 09:37:03 +0100

    init commit

---
+++ componants/NavBar.jsx
@@ -0,0 +1,16 @@
+export default function NavBar() {
+    return (
+    <>
+        <div>
+            <h1>titel</h1>
+        </div>
+        <div>
+            <ul>
+            <li>home</li>
+            <li>order</li>
+            <li>wishlist</li>
+            </ul>
+        </div>
+    </>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import NavBar from '../componants/NavBar'
+function App() {
+  return (
+    <>
+     <NavBar />
+    </>
+  )
+}
+
+export default App
